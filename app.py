4a63be15d481eb1db633d45d4bed5752958d4888
Ubuntu <ubuntu@ip-172-31-30-129.ap-southeast-1.compute.internal>
2023-10-27 09:53:36 +0000

    initial version

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1 style="background-color:blue;">Hello, World ! - Pyflask App 2</h1>'
+
+@app.route('/version')
+def get_version():
+    return '<h1>App version : <b>2.0</b></h1>'
+
+@app.route('/test')
+def get_test():
+    return '<h1>You are accessing /test endpoint</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
