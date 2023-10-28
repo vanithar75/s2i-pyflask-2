eb9b4b9fbb007945512b7acb1f343004a4701e2c
Ubuntu <ubuntu@ip-172-31-30-129.ap-southeast-1.compute.internal>
2023-10-28 02:29:11 +0000

    refactoring

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+        bg_color = "Lime"
+        font_color = "white"
+        return render_template('index.html', bg_color=bg_color, font_color=font_color)
+
+if __name__ == '__main__':
+        app.run(host="0.0.0.0")
+    #margin-top: -100px; 
+    #margin-left: -900px; 
