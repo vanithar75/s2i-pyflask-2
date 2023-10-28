76e03fb71f5cae3b15a34e0ea60b04604a27ba1f
vanithar75 <66339806+vanithar75@users.noreply.github.com>
2023-10-28 11:03:33 +0800

    Update app.py
    updated default port no

--- app/app.py
+++ app/app.py
@@ -7,6 +7,6 @@ def home():
         return render_template('index.html', bg_color=bg_color, font_color=font_color)
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0",port="80")
+        app.run(host="0.0.0.0",port="8080")
     #margin-top: -100px; 
     #margin-left: -900px; 
